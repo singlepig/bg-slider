236c87107a75b46b4a38119dc58603a638abef17
singlepig <gys.nanjing@gmail.com>
2013-09-05 09:58:20 +0800

    用python改写，实现在特定目录下随机选择壁纸进行显示
    Signed-off-by: singlepig <gys.nanjing@gmail.com>

---
+++ biubiu.py
@@ -0,0 +1,16 @@
+#!/user/bin/bash
+#-*- encoding: utf-8 -*-
+
+import os
+import random
+import time
+
+running = True
+
+while running:
+    pic_list = os.listdir('/home/singlepig/pictures/wallpapers')#壁纸目录
+    pic_num = len(pic_list)#获取壁纸目录的壁纸数量
+    filename = random.randint(0,pic_num)#随机选择一个壁纸在pic_list中的序号
+    print(pic_list[filename])
+    os.popen('gsettings set org.gnome.desktop.background picture-uri file:///'+'home/singlepig/pictures/wallpapers/'+pic_list[filename])
+    time.sleep(60*2)#休眠2分钟
