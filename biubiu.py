aa41fc017f136ccdd635c49ab618155ece4a7a2e
singlepig <gys.nanjing@gmail.com>
2013-09-11 08:37:09 +0800

    1、去除打印文件名。 Signed-off-by: singlepig <gys.nanjing@gmail.com>

--- biubiu.py
+++ biubiu.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#/usr/bin/python
 #-*- encoding: utf-8 -*-
 
 import os
@@ -11,6 +11,6 @@ while running:
     pic_list = os.listdir('/home/singlepig/pictures/wallpapers')#壁纸目录
     pic_num = len(pic_list)#获取壁纸目录的壁纸数量
     filename = random.randint(0,pic_num)#随机选择一个壁纸在pic_list中的序号
-    print(pic_list[filename])
+    #print(pic_list[filename])
     os.popen('gsettings set org.gnome.desktop.background picture-uri file:///'+'home/singlepig/pictures/wallpapers/'+pic_list[filename])
     time.sleep(60*2)#休眠2分钟
